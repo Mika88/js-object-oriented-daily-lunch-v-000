c731cfa255da9935711c392001bc11d162361260
Mika88 <mikaspivak@gmail.com>
2019-03-30 15:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
-let mealId = 0;
-let customerId = 0;
-let deliveryId = 0;
 
 class Neighborhood {
   constructor(name) {
@@ -13,3 +10,5 @@ class Neighborhood {
     store.neighborhoods.push(this);
   }
 }
+
+
