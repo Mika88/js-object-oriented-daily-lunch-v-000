de18235976b4b099331d7c8a51d6324f976fe4bc
Mika88 <mikaspivak@gmail.com>
2019-03-30 17:50:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ class Customer {
       return delivery.meal();
     });
   }
-  
+
   totalSpent() {
     this.meals.reduce(reducer).reduce(function (accumulator, currentValue) {
       return accumulator + currentValue.price;
