7a83dfc550700d43ff9513e69207114450af139b
Mika88 <mikaspivak@gmail.com>
2019-03-30 15:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ let neighborhoodId = 0;
 let mealId = 0;
 let customerId = 0;
 let deliveryId = 0;
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+    store.neighborhoods.push(this);
+  }
+}
